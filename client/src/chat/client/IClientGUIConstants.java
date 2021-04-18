3c9440d76b0354fca4f9017215e1ea1253d76c13
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-04-18 21:41:55 +0300

    fonts constants for client window

---
+++ client/src/chat/client/IClientGUIConstants.java
@@ -0,0 +1,9 @@
+package chat.client;
+
+import java.awt.*;
+
+public interface IClientGUIConstants {
+    Font logPageFont = new Font("Dialog", Font.BOLD, 16);
+    Font buttonsFont = new Font("Microsoft New Tai Lue", Font.BOLD, 24);
+    Font logsFont = new Font("Lucida Console", Font.PLAIN, 16);
+}
