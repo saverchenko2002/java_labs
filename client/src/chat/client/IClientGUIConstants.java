cd1b351ca4652d600236488ac122b3bcb0e204d4
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-04-18 22:28:18 +0300

    font changes

--- client/src/chat/client/IClientGUIConstants.java
+++ client/src/chat/client/IClientGUIConstants.java
@@ -5,5 +5,6 @@ import java.awt.*;
 public interface IClientGUIConstants {
     Font logPageFont = new Font("Dialog", Font.BOLD, 16);
     Font buttonsFont = new Font("Microsoft New Tai Lue", Font.BOLD, 24);
-    Font logsFont = new Font("Lucida Console", Font.PLAIN, 16);
+    Font logsFont = new Font("Franklin Gothic Medium", Font.PLAIN, 20);
+    Font confirmFont = new Font("Arial Narrow", Font.BOLD, 14);
 }
