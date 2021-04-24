a1b45c16039792b9d40072a24245550412f25602
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-04-24 14:46:22 +0300

    simplification of request acceptance server side

--- server/src/chat/server/ChatServer.java
+++ server/src/chat/server/ChatServer.java
@@ -1,5 +1,6 @@
 package chat.server;
 
+import chat.database.Database;
 import chat.network.IStatusCodes;
 import chat.network.TCPConnection;
 import chat.network.TCPConnectionListener;
@@ -14,16 +15,13 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServer implements TCPConnectionListener, IStatusCodes {
 
-    public static void main(String[] args) {
-        new ChatServer();
-    }
 
     private final ArrayList<TCPConnection> connections = new ArrayList<>();
-    private static final ConcurrentHashMap<TCPConnection, String> currentConnectionsDatabaseCS = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<String, TCPConnection> currentConnectionsDatabaseSC = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<String, String> database = new ConcurrentHashMap<>();
+    Database database = new Database();
 
-    String[] authorizationInfo = new String[3];
+
+//    String clientsOnline;
+
 
     private ChatServer() {
         System.out.println("Server running");
@@ -47,56 +45,61 @@ public class ChatServer implements TCPConnectionListener, IStatusCodes {
 
     @Override
     public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
-        StringTokenizer str;
-        if (value == null)
-            return;
-        else if (value.equals(DISCONNECT_TOKEN)) {
-            tcpConnection.sendString(DISCONNECT_TOKEN);
-            tcpConnection.disconnect();
-            return;
-        } else
-            str = new StringTokenizer(value, " ");
-        authorizationInfo[0] = str.nextToken();
-        if (authorizationInfo[0].equals(REGISTRATION_TOKEN)) {
-            authorizationInfo[1] = str.nextToken();
-            authorizationInfo[2] = str.nextToken();
-
-            if (database.containsKey(authorizationInfo[1])) {
-                tcpConnection.sendString(EXISTING_LOGIN);
-            } else {
-                database.put(authorizationInfo[1], authorizationInfo[2]);
-                tcpConnection.sendString(REGISTRATION_SUCCESS_TOKEN);
-            }
-
-        } else if (authorizationInfo[0].equals(LOGIN_TOKEN)) {
-            authorizationInfo[1] = str.nextToken();
-            authorizationInfo[2] = str.nextToken();
-
-            if (!database.containsKey(authorizationInfo[1])) {
-                tcpConnection.sendString(NONEXISTENT_LOGIN);
-            } else if (database.get(authorizationInfo[1]).equals(authorizationInfo[2])) {
-                if (currentConnectionsDatabaseCS.containsValue(authorizationInfo[1])) {
-                    tcpConnection.sendString(DUAL_CONNECTION_BLOCK);
-                    return;
+        String[] authentication = authenticationHandler(value);
+        switch (authentication[0]) {
+            case (EMPTY_REQUEST):
+            case (NOT_AUTHENTICATION_REQUEST):
+                break;
+            case (REGISTRATION_TOKEN): {
+                if (database.checkLoginExistence(authentication[1]))
+                    tcpConnection.sendString(EXISTING_LOGIN);
+                else {
+                    database.putNewUser(authentication[1], authentication[2]);
+                    tcpConnection.sendString(REGISTRATION_SUCCESS_TOKEN);
                 }
-                currentConnectionsDatabaseSC.put(authorizationInfo[1], tcpConnection);
-                currentConnectionsDatabaseCS.put(tcpConnection, authorizationInfo[1]);
-                tcpConnection.sendString(TO_CHAT);
-                sendToAllConnection(currentConnectionsDatabaseCS.get(tcpConnection) + " connected");
-            } else
-                tcpConnection.sendString(WRONG_PASSWORD);
-        } else {
-            sendToAllConnection(currentConnectionsDatabaseCS.get(tcpConnection) + value);
+                break;
+            }
+            case (LOGIN_TOKEN): {
+                if (!database.checkLoginExistence(authentication[1])) {
+                    tcpConnection.sendString(NONEXISTENT_LOGIN);
+                    return;
+                } else if (database.checkPasswordMatch(authentication[1], authentication[2])) {
+                    if (database.alreadyOnline(authentication[1])) {
+                        tcpConnection.sendString(DUAL_CONNECTION_BLOCK);
+                        return;
+                    }
+                    tcpConnection.sendString(TO_CHAT);
+                    database.putUserOnlineList(authentication[1], tcpConnection);
+                    sendToAllConnection(database.getLoginByConnection(tcpConnection) + " connected");
+                } else
+                    tcpConnection.sendString(WRONG_PASSWORD);
+                break;
+            }
+        }
+        String[] message = messageHandler(value);
+        switch (message[0]) {
+            case (EMPTY_REQUEST):
+                break;
+            case (SIMPLE_MESSAGE_TOKEN): {
+                sendToAllConnection(database.getLoginByConnection(tcpConnection) + message[1]);
+                break;
+            }
+            case (PRIVATE_MESSAGE_TOKEN): {
+                break;
+            }
         }
     }
 
     @Override
     public synchronized void onDisconnect(TCPConnection tcpConnection) {
         connections.remove(tcpConnection);
-        sendToAllConnection(currentConnectionsDatabaseCS.get(tcpConnection) + " disconnected");
-        String trashCan = currentConnectionsDatabaseCS.get(tcpConnection);
-        currentConnectionsDatabaseCS.remove(tcpConnection);
-        currentConnectionsDatabaseSC.remove(trashCan);
+//        sendToAllConnection(currentConnectionsDatabaseCS.get(tcpConnection) + " disconnected");
+//        String trashCan = currentConnectionsDatabaseCS.get(tcpConnection);
+//        currentConnectionsDatabaseCS.remove(tcpConnection);
+//        currentConnectionsDatabaseSC.remove(trashCan);
+//        clientsOnline = ONLINE_LIST_REFRESH + " ";
+//        clientsOnline += currentConnectionsDatabaseSC.keySet().toString();
+//        sendToAllConnection(clientsOnline);
     }
 
     @Override
@@ -106,5 +109,64 @@ public class ChatServer implements TCPConnectionListener, IStatusCodes {
 
     private void sendToAllConnection(String value) {
         for (TCPConnection connection : connections) connection.sendString(value);
+    }
+
+    private String[] authenticationHandler(String value) {
+        String[] authentication;
+        StringTokenizer str;
+        if (value == null) {
+            authentication = new String[1];
+            authentication[0] = EMPTY_REQUEST;
+        } else {
+            authentication = new String[3];
+            str = new StringTokenizer(value, " ");
+            authentication[0] = str.nextToken();
+            switch (authentication[0]) {
+                case (REGISTRATION_TOKEN):
+                    authentication[1] = str.nextToken();
+                    authentication[2] = str.nextToken();
+                    break;
+                case (LOGIN_TOKEN): {
+                    authentication[1] = str.nextToken();
+                    authentication[2] = str.nextToken();
+                    break;
+                }
+                default: {
+                    authentication[0] = NOT_AUTHENTICATION_REQUEST;
+                    break;
+                }
+            }
+        }
+        return authentication;
+    }
+
+    private String[] messageHandler(String value) {
+        String[] message;
+        StringTokenizer str;
+        if (value == null) {
+            message = new String[1];
+            message[0] = EMPTY_REQUEST;
+        } else {
+            message = new String[2];
+            str = new StringTokenizer(value, " ");
+            message[0] = str.nextToken();
+            message[1] = "";
+            switch (message[0]) {
+                case (SIMPLE_MESSAGE_TOKEN):
+                    while (str.hasMoreTokens()) {
+                        message[1] += str.nextToken();
+                        message[1] += " ";
+                    }
+                    break;
+                case (PRIVATE_MESSAGE_TOKEN):
+                    break;
+            }
+        }
+        return message;
+    }
+
+
+    public static void main(String[] args) {
+        new ChatServer();
     }
 }
