a01fda245c6b5a05a1abcfcdce2448abbad62c21
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-04-24 21:12:36 +0300

    pop and convertation to string funcs

--- database/src/chat/database/Database.java
+++ database/src/chat/database/Database.java
@@ -2,19 +2,21 @@ package chat.database;
 
 import chat.network.TCPConnection;
 
+import java.io.*;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class Database {
+public class Database implements Serializable {
     private static final ConcurrentHashMap<String, String> database = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<TCPConnection, String> currentUsersConnectionLogin = new ConcurrentHashMap<>();
     private static final ConcurrentHashMap<String, TCPConnection> currentUsersLoginConnection = new ConcurrentHashMap<>();
+    private static final long serialVersionUID = 1L;
 
     public Database() {
 
     }
 
     public void putNewUser(String login, String password) {
-        database.put(login,password);
+        database.put(login, password);
     }
 
     public boolean checkLoginExistence(String login) {
@@ -30,11 +32,23 @@ public class Database {
         currentUsersLoginConnection.put(login, tcpConnection);
     }
 
+    public void popUserOnlineList(TCPConnection tcpConnection) {
+        String login = currentUsersConnectionLogin.get(tcpConnection);
+        currentUsersConnectionLogin.remove(tcpConnection);
+        currentUsersLoginConnection.remove(login);
+    }
+
     public boolean alreadyOnline(String login) {
         return currentUsersConnectionLogin.containsValue(login);
     }
 
     public String getLoginByConnection(TCPConnection tcpConnection) {
         return currentUsersConnectionLogin.get(tcpConnection);
+    }
+
+    public String onlineList() {
+        String list = currentUsersLoginConnection.keySet().toString();
+        list = list.replace("]","").replace("[","").replace(",", "");
+        return list;
     }
 }
