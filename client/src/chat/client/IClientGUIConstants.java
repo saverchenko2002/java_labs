3a5081f76f36ba89b4135b95c2aa78736eb59a83
SIARHEI SAVERCHANKA <lordofdarkness011@tut.by>
2021-04-24 17:12:54 +0300

    new constants

--- client/src/chat/client/IClientGUIConstants.java
+++ client/src/chat/client/IClientGUIConstants.java
@@ -7,4 +7,7 @@ public interface IClientGUIConstants {
     Font buttonsFont = new Font("Microsoft New Tai Lue", Font.BOLD, 24);
     Font logsFont = new Font("Franklin Gothic Medium", Font.PLAIN, 20);
     Font confirmFont = new Font("Arial Narrow", Font.BOLD, 14);
+
+    String DUAL_ERROR = "CURRENT_USER_IS_ALREADY_ON_A_SERVER";
+    String ERROR_TITLE = "ERROR";
 }
